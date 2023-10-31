1bd84ef066adb38cf704edecfa8d69ef45c182b9
ydrafate <149504804+ydrafate@users.noreply.github.com>
2023-10-31 14:04:11 +0000

    urls fix + new blog hhh

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import write_blog, view_blogs
 
 urlpatterns = [
     path('write/', write_blog, name='write'),
-    path('view/', view_blogs, name='list'),
+    path('', view_blogs, name='list'),
     # other patterns...
 ]
